cc12927efe16a909f2139e2009351d7b6daa0ea9
Mateo <marcelicmateo@gmail.com>
2021-08-15 12:01:30 +0200

    printer_init: added printer init function
    imported cups, and add dependency to pycups

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 #!.env/bin/python
+import cups
+import json
 
 
 def main():
     return 0
 
 
+def open_cups():
+    return 0
+
+
+def printer_init():
+    conn = cups.Connection()
+    if conn.getDefault() is None:
+        log.warning(
+            """
+        No default printer is set.
+        Please set defaulty server printer.
+        Opening CUPS@localhost"""
+        )
+        open_cups()
+    return 0
+
+
 if __name__ == "__main__":
+    printer_init()
     main()
