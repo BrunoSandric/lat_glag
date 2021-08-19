d3a96248c2d8a52e01b04c91befdd1550ab0cdb2
BrunoSandric <bruno.sandric@gmail.com>
2021-08-19 19:49:19 +0200

    ADD GUI
    Added GUI

---
+++ GUI.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+import dash_design_kit as ddk
+import dash_core_components as dcc
+
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+colors = {
+    'background': '#BEBEBE',
+    'text': '#000000'
+}
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Prevoditelj latinice u glagoljicu',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    )
+
+
+
+],
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
